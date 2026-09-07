fc23e318f4ff5e2c0211ef0b70e6702af92032e2
Janith Priyankara <janithpriyankara233@gmail.com>
2026-09-07 09:40:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/real_estate_portal/RealEstatePortalApplication.java
@@ -0,0 +1,13 @@
+package real_estate_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstatePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstatePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/real_estate_portal/RealEstatePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package real_estate_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstatePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
